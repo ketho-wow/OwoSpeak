5d50adf5191fc4515c02f83db526010b49cffeb8
Ketho <sghs17@outlook.com>
2026-02-15 00:45:03 +0100

    wewwwitten f-fow Midnighht and BCC Anniwewsawy >w< added options gui to f-fiwtew chat chawnews owo

--- OwoSpeak.lua
+++ OwoSpeak.lua
@@ -1,233 +1,148 @@
-local owos = {
-	"OwO",
-	"owo",
-	"UwU",
-	"uwu",
-	"^w^",
-	">w<",
-	"(´•ω•`)",
-}
+local NAME = ...
+local ACR = LibStub("AceConfigRegistry-3.0")
+local ACD = LibStub("AceConfigDialog-3.0")
+
+OwoSpeak = {}
 
 local defaults = {
 	enabled = true,
-	guild = true,
-	officer = true,
-	whisper = true,
+	SAY = true,
+	YELL = true,
+	EMOTE = false,
+	PARTY = true,
+	RAID = false,
+	INSTANCE_CHAT = true,
+	GUILD = true,
+	OFFICER = false,
+	WHISPER = false,
 }
 
-local blockedChannelsDefaults = {
-	"NewcomerChat",
+local chat = {
+	"SAY",
+	"YELL",
+	"INSTANCE_CHAT",
+	"EMOTE",
+	"PARTY",
+	"RAID",
+	"GUILD",
+	"OFFICER",
+	"WHISPER",
 }
 
-local db
-local blockedChannels
-local hyperlinks = {}
+local channels = {}
 
-local function OnEvent(self, event, addon)
-	if addon == "OwoSpeak" then
-		OwoSpeakDB = OwoSpeakDB or CopyTable(defaults)
-		OwoSpeakDBBlockedChannels = OwoSpeakDBBlockedChannels or CopyTable(blockedChannelsDefaults)
-		blockedChannels = OwoSpeakDBBlockedChannels
-		db = OwoSpeakDB
+local options = {
+	type = "group",
+	name = "|TInterface/AddOns/OwoSpeak/icon.png:20:20:-8:2|t"..NAME,
+	get = function(i) return OwoSpeak.db[i[#i]] end,
+	set = function(i, v) OwoSpeak.db[i[#i]] = v end,
+	args = {
+		enabled = {
+			type = "toggle", order = 1,
+			width = "full",
+			name = ENABLE,
+		},
+		chat = {
+			type = "group", order = 2,
+			name = CHAT,
+			inline = true,
+			disabled = function() return not OwoSpeak.db.enabled end,
+			args = {},
+		},
+		channels = {
+			type = "group", order = 3,
+			name = CHANNELS,
+			inline = true,
+			disabled = function() return not OwoSpeak.db.enabled end,
+			args = {},
+		},
+	},
+}
+
+-- ChatTypeInfo is not immediately available
+C_Timer.After(1, function()
+	for k, v in pairs(chat) do
+		local info = ChatTypeInfo[v]
+		local color = CreateColor(info.r, info.g, info.b)
+		options.args.chat.args[v] = {
+			type = "toggle", order = k,
+			name = function()
+				-- make it possible to gray out the channel names
+				return OwoSpeak.db.enabled and color:WrapTextInColorCode(_G[v]) or _G[v]
+			end,
+		}
+	end
+end)
+
+local f = CreateFrame("Frame")
+
+function f:OnEvent(event, ...)
+	self[event](self, ...)
+end
+
+function f:ADDON_LOADED(addon)
+	if addon == NAME then
+		OwoSpeakDB3 = OwoSpeakDB3 or {}
 		for k, v in pairs(defaults) do
-			if db[k] == nil then
-				db[k] = v
+			-- allow adding new options this way
+			if OwoSpeakDB3[k] == nil then
+				OwoSpeakDB3[k] = v
 			end
 		end
+		OwoSpeak.db = OwoSpeakDB3
+		ACR:RegisterOptionsTable(NAME, options)
+		ACD:AddToBlizOptions(NAME, NAME)
 		self:UnregisterEvent("ADDON_LOADED")
 	end
 end
 
-local f = CreateFrame("Frame")
-f:RegisterEvent("ADDON_LOADED")
-f:SetScript("OnEvent", OnEvent)
-
--- owo1, owo2, etc pwacehowdews
-local function ReplaceLink(s)
-	tinsert(hyperlinks, s)
-	return "owo"..#hyperlinks
+function f:PLAYER_ENTERING_WORLD(isLogin, isReload)
+	if isReload then -- refresh after a /reload
+		self:CHANNEL_UI_UPDATE()
+	end
 end
 
-local function RestoreLink(s)
-	local n = tonumber(s:match("%d"))
-	return hyperlinks[n]
-end
+function f:CHANNEL_UI_UPDATE()
+	wipe(channels)
+	local list = {GetChannelList()}
 
-local channelOptions = {
-	GUILD = function() return db.guild end,
-	OFFICER = function() return db.officer end,
-	WHISPER = function() return db.whisper end,
-}
+	for i = 1, #list, 3 do
+		local name = list[i+1]
+		channels[list[i]] = strfind(name, "Community:") and ChatFrame_ResolveChannelName(name) or name
+	end
 
-local function ShouldOwo(chatType)
-	if db.enabled then
-		if channelOptions[chatType] then
-			return channelOptions[chatType]()
+	for i = 1, MAX_WOW_CHAT_CHANNELS do
+		if channels[i] then
+			local info = ChatTypeInfo["CHANNEL"..i]
+			local color = CreateColor(info.r, info.g, info.b)
+			local text = format("%d. %s", i, channels[i])
+			options.args.channels.args["CHANNEL"..i] = {
+				type = "toggle", order = i,
+				name = function() return OwoSpeak.db.enabled and color:WrapTextInColorCode(text) or text end,
+			}
 		else
-			return true
+			options.args.channels.args["CHANNEL"..i] = nil
 		end
 	end
+	ACR:NotifyChange(NAME)
 end
 
-local function ShouldOwoTwo(chatType, channel)
-	if chatType == "CHANNEL" then
-		local id, channelName = GetChannelName(channel)
-
-		for key, value in pairs(blockedChannels) do
-			if channelName == value then
-				-- print("Found " .. channelName .. " in blockedChannels table.  Do not owo")
-				return false
-			end
-		end
-	end
-	-- print("Did not find " .. channelName .. " in blockedChannels table.  owo away!")
-	return true
-end
-
-local makeowo
-
-local function owo(msg, chatType, language, channel)
-	-- im so sowwy meo
-	if msg == "GHI2ChannelReadyCheck" then
-		return
-	end
-	if ShouldOwo(chatType) and ShouldOwoTwo(chatType, channel) then
-		wipe(hyperlinks)
-		local owo = owos[random(#owos)]
-		local whatsthis = random(10)
-		-- tempowawiwy wepwace winks wif owos
-		local s = msg:gsub("|c.-|r", ReplaceLink)
-		-- wepwace waid mawkers
-		-- pwacehowders
-		s = s:gsub("{.-}", ReplaceLink)
-		s = s:gsub("([lr])([%S]*s?)", function(l, following)
-		    if l == 'r' and following == 's' then
-		        return 'r' .. following
-		    elseif l == 'R' and following == 's' then
-		        return 'R' .. following
-		    else
-		        return 'w' .. following
-		    end
-		end)
-
-		s = s:gsub("([LR])([%S]*S?)", function(L, following)
-		    if L == 'R' and following == 'S' then
-		        return 'R' .. following
-		    elseif L == 'L' then
-		        return 'W' .. following
-		    else
-		        return 'W' .. following
-		    end
-		end)
-		if whatsthis <= 5 then
-			s = s:gsub("U([^VW])", "UW%1")
-			s = s:gsub("u([^vw])", "uw%1")
-		end
-		s = s:gsub("ith " , "if ")
-		s = s:gsub("([fps])([aeio]%w+)", "%1w%2") or s
-		s = s:gsub("n([aeiou]%w)", "ny%1") or s
-		s = s:gsub(" th", " d") or s
-		s = format(" %s ", s)
-		for k in gmatch(s, "%a+") do
-			if random(12) == 1 then
-				local firstChar = k:sub(1, 1)
-				s = s:gsub(format(" %s ", k), format(" %s-%s ", firstChar, k))
-			end
-		end
-		s = s:trim()
-		s = whatsthis == 1 and s.." "..owo or s:gsub("!$", " "..owo)
-		-- pwease owo wesponsibwy
-		s = #s <= 255 and s:gsub("owo%d", RestoreLink) or msg
-		makeowo(s, chatType, language, channel)
-	else
-		makeowo(msg, chatType, language, channel)
-	end
-end
-
-if C_ChatInfo.SendChatMessage then
-	makeowo = C_ChatInfo.SendChatMessage
-	C_ChatInfo.SendChatMessage = owo
-else
-	makeowo = SendChatMessage
-	SendChatMessage = owo
-end
-
-local EnabledMsg = {
-	[true] = "|cffADFF2FEnabwed|r",
-	[false] = "|cffFF2424Disabwed|r",
-}
-
-local function PrintMessage(msg)
-	print("OwoSpeak: "..msg)
-end
-
-SLASH_OWOSPEAK1 = "/owo"
-SLASH_OWOSPEAK2 = "/owospeak"
-
-local function tablefind(tab,el)
-    for index, value in pairs(tab) do
-        if value == el then
-            return index
-        end
+function OwoSpeak:shouldowo(chatType, target)
+    if chatType == "CHANNEL" then
+        return OwoSpeak.db[chatType..target]
+    else
+        return OwoSpeak.db[chatType]
     end
 end
 
-SlashCmdList.OWOSPEAK = function(msg)
-	if msg == "guild" then
-		db.guild = not db.guild
-		PrintMessage("Guild - "..EnabledMsg[db.guild])
-	elseif msg == "officer" then
-		db.officer = not db.officer
-		PrintMessage("Officer - "..EnabledMsg[db.officer])
-	elseif msg == "whisper" then
-		db.whisper = not db.whisper
-		PrintMessage("Whisper - "..EnabledMsg[db.whisper])
-	elseif string.find(msg, "add") then
-		local exploded = {}
-		for substring in string.gmatch(msg, "[^%s]+") do
-		   table.insert(exploded, substring)
-		end
-		if exploded[2] then
-			table.insert(blockedChannels, exploded[2])
-			PrintMessage("Added " .. exploded[2] .. " to the blocked channel list.")
-		else
-			PrintMessage("You must provide a channel name to block.")
-		end
-	elseif string.find(msg, "remove") then
-		local exploded = {}
-		local foundAndRemoved = false
-		for substring in string.gmatch(msg, "[^%s]+") do
-		   table.insert(exploded, substring)
-		end
-		if exploded[2] then
-			for key, value in pairs(blockedChannels) do
-				if value == exploded[2] then
-					PrintMessage("Removed " .. exploded[2] .. " from the blocked channels list.")
-					table.remove(blockedChannels, tablefind(blockedChannels, exploded[2]))
-					foundAndRemoved = true
-				end
-			end
-			if foundAndRemoved == false then
-				PrintMessage("Could not find the specified channel in the blocked channels list.")
-			end
-		else
-			PrintMessage("You must provide a channel name to unblock.")
-		end
-	elseif msg == "blocked" then
-		PrintMessage("Currently blocked channels:")
-		for key, value in pairs(blockedChannels) do
-			print(value)
-		end
-	elseif msg == "help" then
-		PrintMessage("Available commands:")
-		print("/owo guild - enable/disable guild chat owospeak")
-		print("/owo officer - enable/disable officer chat owospeak")
-		print("/owo whisper - enable/disable whisper owospeak")
-		print("/owo add <channel name> - prevent owospeak in a specific channel")
-		print("/owo remove <channel name> - re-enable owospeak in a blocked channel (see /owo add)")
-		print("/owo blocked - print list of currently blocked channels")
-	else
-		db.enabled = not db.enabled
-		PrintMessage(EnabledMsg[db.enabled])
-	end
+f:RegisterEvent("ADDON_LOADED")
+f:RegisterEvent("PLAYER_ENTERING_WORLD")
+f:RegisterEvent("CHANNEL_UI_UPDATE")
+f:SetScript("OnEvent", f.OnEvent)
+
+SLASH_OWOSPEAK1 = "/owospeak"
+SLASH_OWOSPEAK2 = "/owo"
+SlashCmdList.OWOSPEAK = function()
+	ACD:SetDefaultSize(NAME, 420, 420)
+	ACD:Open(NAME)
 end
---
+++ owo.lua
@@ -0,0 +1,97 @@
+local emoticons = {
+	"owo",
+	"uwu",
+	">w<",
+	":3",
+}
+
+local links = {}
+
+local function ReplaceLink(s)
+	tinsert(links, s)
+	return "#@#"..#links -- pwacehowdews fow winks
+end
+
+local function RestoreLinks(s)
+	local n = tonumber(s:match("%d+"))
+	return links[n]
+end
+
+local function SubstituteLinks(s)
+    wipe(links)
+    s = s:gsub("|c.-|r", ReplaceLink)
+    s = s:gsub("{.-}", ReplaceLink)
+    return s
+end
+
+-- wuv u chatgptee
+function OwoSpeak.owoify(text, stutterChance, emoticonChance)
+    stutterChance = stutterChance or 0.15
+    emoticonChance = emoticonChance or 0.4
+    -- do not owo the winks
+    text = SubstituteLinks(text)
+
+    -- the -> da
+    text = text:gsub("%f[%a]the%f[%A]", "da")
+    text = text:gsub("%f[%a]The%f[%A]", "Da")
+
+    -- love -> wuv
+    text = text:gsub("%f[%a]love%f[%A]", "wuv")
+    text = text:gsub("%f[%a]Love%f[%A]", "Wuv")
+
+    -- consonant clusters
+    local clusters = {
+        {"na", "nya"}, {"Na", "Nya"},
+        {"ne", "nye"}, {"Ne", "Nye"},
+        {"ni", "nyi"}, {"Ni", "Nyi"},
+        {"no", "nyo"}, {"No", "Nyo"},
+        {"nu", "nyu"}, {"Nu", "Nyu"},
+    }
+
+    for _, rule in ipairs(clusters) do
+        text = text:gsub(rule[1], rule[2])
+    end
+
+    -- consonant softening
+    text = text:gsub("th", "d")
+    text = text:gsub("Th", "D")
+
+    -- consonant substitutions
+    text = text:gsub("r", "w")
+    text = text:gsub("l", "w")
+    text = text:gsub("R", "W")
+    text = text:gsub("L", "W")
+
+    -- stutter
+    text = text:gsub("(%f[%a])([A-Za-z])", function(boundary, letter)
+        if random() < stutterChance then
+            return boundary..letter.."-"..letter
+        else
+            return boundary..letter
+        end
+    end)
+
+    -- exclamation
+    text = text:gsub("!", "!!!")
+
+    -- emoticons
+    if math.random() < emoticonChance then
+        text = text.." "..emoticons[random(#emoticons)]
+    end
+
+    text = text:gsub("#@#%d+", RestoreLinks)
+    return text
+end
+
+local makeowo = C_ChatInfo.SendChatMessage
+
+---@diagnostic disable-next-line: duplicate-set-field
+C_ChatInfo.SendChatMessage = function(message, chatType, languageID, target)
+    if OwoSpeak:shouldowo(chatType, target) then
+        local owofied = OwoSpeak.owoify(message)
+        if #owofied < 255 then
+            message = owofied
+        end
+    end
+    makeowo(message, chatType, languageID, target)
+end
